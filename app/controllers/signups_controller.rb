936949d2a687d8dc962a35785c7e5c6a11f3e1dd
TJKresch <tjkresch@gmail.com>
2013-06-18 19:41:18 -0700

    changing name

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,4 +1,17 @@
 class SignupsController < InheritedResources::Base
-	def home
-	end
+
+  def create
+    @signup = Signup.new(params[:signup])
+
+    respond_to do |format|
+      if @signup.save
+        format.html { redirect_to new_signup_path, notice: 'Thank you for registering!' }
+        format.json { render json: @signup, status: :created, location: @signup }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @signup.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
